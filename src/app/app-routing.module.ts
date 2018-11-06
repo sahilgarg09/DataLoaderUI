b8eb32e5fb6844c402aa7f6883393ecb31dfdf43
Das <aritdas@deloitte.com>
2018-11-06 13:22:20 +0530

    Add auth guard & show/hide layout based on login status.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,38 @@ import { NgModule } from '@angular/core';
 //import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 
+//Add layouts
+import { LoginLayoutComponent } from './layouts/login-layout/login-layout.component';
+import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
+
+
 //Add components
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-
+import { AuthGuard } from './auth/auth.guard';
 
 const routes: Routes = [
 	{ path: '', redirectTo: '/login', pathMatch: 'full' },
-  { path: 'login', component: LoginComponent},
-  { path: 'dashboard', component: DashboardComponent }
+	{ 
+		path: '', 
+		component: LoginLayoutComponent,
+		children: [{
+			path: 'login',
+			component: LoginComponent
+		}]
+	},
+	{ 
+		path: '', 
+		component: HomeLayoutComponent, 
+		canActivate: [AuthGuard],
+		children: [
+			{
+				path: 'dashboard',
+				component: DashboardComponent
+			}
+		]
+	}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,28 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { SidenavbarComponent } from './components/sidenavbar/sidenavbar.component';
+
+import { AuthGuard } from './auth/auth.guard';
+import { AuthService } from './auth/auth.service';
+import { HomeLayoutComponent } from './layouts/home-layout/home-layout.component';
+import { LoginLayoutComponent } from './layouts/login-layout/login-layout.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    SidenavbarComponent,
+    HomeLayoutComponent,
+    LoginLayoutComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [AuthService, AuthGuard],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
