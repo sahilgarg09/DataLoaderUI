cdaccca7a6436e7a1d39bdea119e7b8335fd8f05
Ashutosh <ashutjha@deloitte.com>
2018-11-29 08:43:18 +0530

    sidebar active tab

--- src/main/springbot-angularApp/src/app/components/sidenavbar/sidenavbar.component.ts
+++ src/main/springbot-angularApp/src/app/components/sidenavbar/sidenavbar.component.ts
@@ -5,11 +5,33 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './sidenavbar.component.html',
   styleUrls: ['./sidenavbar.component.css']
 })
+
+
 export class SidenavbarComponent implements OnInit {
+
+  queries: Query[] = [
+    { id: 1, name: 'Export', shortName: 'EX', url: '/soql-query', class: 'nav-item' },
+    { id: 2, name: 'Insert', shortName: 'IN', url: '', class: 'nav-item' },
+    { id: 3, name: 'Upsert', shortName: 'UP', url: '', class: 'nav-item' },
+    { id: 4, name: 'Delete', shortName: 'DE', url: '', class: 'nav-item' }
+  ];
+  selectedQuery: Query;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSelectLink(query: Query): void {
+    this.selectedQuery = query;
+  }
+
 }
+export class Query {
+  id: number;
+  name: string;
+  shortName: string;
+  url: string;
+  class: string;
+}
+
